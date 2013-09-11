8aec88a753d4e94405309ed719fa8347481d7aad
Andrew Dunham <andrew@du.nham.ca>
2013-09-11 14:03:09 -0700

    Modernize pythonrc.py, courtesy of mitsuhiko

--- link/pythonrc.py
+++ link/pythonrc.py
@@ -1,56 +1,69 @@
-"""
-This file is executed when the Python interactive shell is started if
-$PYTHONSTARTUP is in your environment and points to this file. It's just
-regular Python commands, so do what you will. Your ~/.inputrc file can greatly
-complement this file.
-
-"""
-import os
+# Needs to stay here so sys.ps1 doesn't get modified.
 import sys
 
-try:
-    import readline
-    import rlcompleter
+def _init():
     import atexit
-except ImportError:
-    print("You need readline, rlcompleter, and atexit")
+    import os
+    import readline
+    import types
+    import time
+    import uuid
+    import pprint
+    import hashlib
+    import datetime
+    try:
+        import __builtin__
+    except ImportError:
+        import builtins as __builtin__
+
+    histdir = os.path.expanduser('~/.pyhist')
+    try:
+        os.makedirs(histdir)
+    except OSError:
+        pass
+
+    def _b(x):
+        if not isinstance(x, bytes):
+            x = x.encode('utf-8')
+        return x
+
+    histfile = os.path.join(histdir, hashlib.sha1(
+        os.path.normpath(_b(os.path.abspath(sys.prefix)))).hexdigest())
+
+    try:
+        readline.read_history_file(histfile)
+    except IOError:
+        pass
+
+    atexit.register(readline.write_history_file, histfile)
 
 
-# Make this work properly in Darwin and Linux
-# NOTE: commented out since this screws with Tab-indenting
-# if 'libedit' in readline.__doc__:
-#     readline.parse_and_bind("bind ^I rl_complete")
-# else:
-#     readline.parse_and_bind("tab: complete")
-
-class Completer(object):
-    def __init__(self):
-        # Enable a History
-        self.HISTFILE = os.path.expanduser("%s/.pyhistory" % os.environ["HOME"])
-
-        # Read the existing history if there is one
-        if os.path.exists(self.HISTFILE):
-            readline.read_history_file(self.HISTFILE)
-
-        # Set maximum number of items that will be written to the history file
-        readline.set_history_length(300)
-        atexit.register(self.savehist)
-
-    def savehist(self):
-        import readline
-        readline.write_history_file(self.HISTFILE)
+    def _magic_uuid(val=None):
+        if val is None:
+            return uuid.uuid4()
+        elif isinstance(val, uuid.UUID):
+            return val
+        elif len(val) == 16:
+            return uuid.UUID(bytes=val)
+        return uuid.UUID(val)
 
 
-c = Completer()
+    helpers = types.ModuleType('helpers')
+    helpers.histfile = histfile
+    helpers.pp = pprint.pprint
+    helpers.uuid = _magic_uuid
+    helpers.UUID = uuid.UUID
+    helpers.uuid3 = uuid.uuid3
+    helpers.uuid4 = uuid.uuid4
+    helpers.uuid5 = uuid.uuid5
+    helpers.dt = datetime.datetime
+    helpers.datetime = datetime.datetime
+    helpers.td = datetime.timedelta
+    helpers.timedelta = datetime.timedelta
+    helpers.time = time.time
+    __builtin__.h = helpers
 
-WELCOME = ''
-
-# Color Support
-class TermColors(dict):
-    """Gives easy access to ANSI color codes. Attempts to fall back to no color
-    for certain TERM values. (Mostly stolen from IPython.)"""
-
-    COLOR_TEMPLATES = (
+    COLORS = dict([
         ("Black"       , "0;30"),
         ("Red"         , "0;31"),
         ("Green"       , "0;32"),
@@ -68,125 +81,16 @@ class TermColors(dict):
         ("LightCyan"   , "1;36"),
         ("White"       , "1;37"),
         ("Normal"      , "0"),
-    )
+    ])
 
     NoColor = ''
-    _base  = '\001\033[%sm\002'
+    BaseTemplate  = '\001\033[%sm\002'
 
-    def __init__(self):
-        if os.environ.get('TERM') in ('xterm-color', 'xterm-256color', 'linux',
-                                    'screen', 'screen-256color', 'screen-bce'):
-            self.update(dict([(k, self._base % v) for k,v in self.COLOR_TEMPLATES]))
-        else:
-            self.update(dict([(k, self.NoColor) for k,v in self.COLOR_TEMPLATES]))
+    for k in COLORS.keys():
+        COLORS[k] = BaseTemplate % (COLORS[k],)
 
+    sys.ps1 = '%s>>> %s' % (COLORS['Green'], COLORS['Normal'])
+    #sys.ps2 = '%s... %s' % (COLORS['Red'], COLORS['Normal'])
 
-_c = TermColors()
-
-
-# Enable Color Prompts
-sys.ps1 = '%s>>> %s' % (_c['Green'], _c['Normal'])
-sys.ps2 = '%s... %s' % (_c['Red'], _c['Normal'])
-
-# Enable Pretty Printing for stdout
-def my_displayhook(value):
-    if value is not None:
-        try:
-            import __builtin__
-            __builtin__._ = value
-        except ImportError:
-            __builtins__._ = value
-
-        import pprint
-        pprint.pprint(value)
-        del pprint
-
-sys.displayhook = my_displayhook
-
-# Django Helpers
-def SECRET_KEY():
-    "Generates a new SECRET_KEY that can be used in a project settings file."
-
-    from random import choice
-    return ''.join(
-            [choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)')
-                for i in range(50)])
-
-# If we're working with a Django project, set up the environment
-if 'DJANGO_SETTINGS_MODULE' in os.environ:
-    from django.db.models.loading import get_models
-    from django.test.client import Client
-    from django.test.utils import setup_test_environment, teardown_test_environment
-    from django.conf import settings as S
-
-    class DjangoModels(object):
-        """Loop through all the models in INSTALLED_APPS and import them."""
-        def __init__(self):
-            for m in get_models():
-                setattr(self, m.__name__, m)
-
-    A = DjangoModels()
-    C = Client()
-
-    WELCOME += """%(Green)s
-    Django environment detected.
-* Your INSTALLED_APPS models are available as `A`.
-* Your project settings are available as `S`.
-* The Django test client is available as `C`.
-%(Normal)s""" % _c
-
-    setup_test_environment()
-    S.DEBUG_PROPAGATE_EXCEPTIONS = True
-
-    WELCOME += """%(LightPurple)s
-Warning: the Django test environment has been set up; to restore the
-normal environment call `teardown_test_environment()`.
-
-Warning: DEBUG_PROPAGATE_EXCEPTIONS has been set to True.
-%(Normal)s""" % _c
-
-# Start an external editor with \e
-# http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/438813/
-
-EDITOR = os.environ.get('EDITOR', 'vim')
-EDIT_CMD = '\e'
-
-from tempfile import mkstemp
-from code import InteractiveConsole
-
-class EditableBufferInteractiveConsole(InteractiveConsole):
-    def __init__(self, *args, **kwargs):
-        self.last_buffer = [] # This holds the last executed statement
-        InteractiveConsole.__init__(self, *args, **kwargs)
-
-    def runsource(self, source, *args):
-        self.last_buffer = [ source.encode('latin-1') ]
-        return InteractiveConsole.runsource(self, source, *args)
-
-    def raw_input(self, *args):
-        line = InteractiveConsole.raw_input(self, *args)
-        if line == EDIT_CMD:
-            fd, tmpfl = mkstemp('.py')
-            os.write(fd, b'\n'.join(self.last_buffer))
-            os.close(fd)
-            os.system('%s %s' % (EDITOR, tmpfl))
-            line = open(tmpfl).read()
-            os.unlink(tmpfl)
-            tmpfl = ''
-            lines = line.split( '\n' )
-            for i in range(len(lines) - 1):
-                self.push( lines[i] )
-
-            line = lines[-1]
-
-        return line
-
-# clean up namespace
-del sys
-
-c = EditableBufferInteractiveConsole(locals=locals())
-c.interact(banner=WELCOME)
-
-# Exit the Python shell on exiting the InteractiveConsole
-import sys
-sys.exit()
+_init()
+del _init
